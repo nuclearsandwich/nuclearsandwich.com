9c4ef40d6def89814426533308b5c6e7917cc130
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-02-01 11:51:04 -0800

    Pygments.rb hates Archlinux so fallback to Albino.
    I got tired of sitting on articles until I could make RubyPython happy
    on Archlinxu. I've dropped back to Albino for the time being and
    hopefully I'll have time to figure out wtf with RubyPython before I have
    time to end world hunger and whatnot.

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -1,9 +1,4 @@
-require 'pygments'
-require 'fileutils'
-require 'digest/md5'
-
-PYGMENTS_CACHE_DIR = File.expand_path('../../.pygments-cache', __FILE__)
-FileUtils.mkdir_p(PYGMENTS_CACHE_DIR)
+require 'albino'
 
 module HighlightCode
   def highlight(str, lang)
@@ -16,19 +11,9 @@ module HighlightCode
   end
 
   def pygments(code, lang)
-    if defined?(PYGMENTS_CACHE_DIR)
-      path = File.join(PYGMENTS_CACHE_DIR, "#{lang}-#{Digest::MD5.hexdigest(code)}.html")
-      if File.exist?(path)
-        highlighted_code = File.read(path)
-      else
-        highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8'})
-        File.open(path, 'w') {|f| f.print(highlighted_code) }
-      end
-    else
-      highlighted_code = Pygments.highlight(code, :lexer => lang, :formatter => 'html', :options => {:encoding => 'utf-8'})
-    end
-    highlighted_code
+    Albino.colorize code, lang, "html", "utf-8"
   end
+
   def tableize_code (str, lang = '')
     table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
     code = ''
