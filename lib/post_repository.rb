198404f3ecaea7bacfe48eb0c9cd3418effdac12
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-09-03 18:50:27 -0700

    PostRepository generates html for blog index.

--- lib/post_repository.rb
+++ lib/post_repository.rb
@@ -20,4 +20,17 @@ class PostRepository
   def each *args, &block
     @posts.each *args, &block
   end
+
+  def index_content
+    builder = ""
+    each do |post|
+      builder << <<-HTML
+<div class="article-link">
+  <h1><a href="/blog/#{post.html_filename}">#{post.title}</a></h1>
+  <h2>Published: #{post.date.strftime "%B %Y"}</h2>
+</div>
+HTML
+    end
+    builder
+  end
 end
