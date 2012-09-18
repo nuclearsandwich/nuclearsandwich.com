a50f233c678f89896878188ddca3cfa30e3b8700
Steven! Ragnarök <steven@nuclearsandwich.com>
2012-09-17 23:36:01 -0700

    Sort articles by year.

--- lib/post_repository.rb
+++ lib/post_repository.rb
@@ -30,13 +30,18 @@ class PostRepository
 
   def index_content
     builder = ""
-    each do |post|
-      builder << <<-HTML
-<div class="article-link">
-  <h1><a href="/blog/#{post.html_filename}">#{post.title}</a></h1>
-  <h2>Published: #{post.date.strftime "%B %Y"}</h2>
-</div>
+    years = @posts.map { |p| p.date.year }.uniq
+    years.each do |year|
+      builder << %Q(<section class="year" id="#{year}">\n  <h1>#{year}</h1>)
+      @posts.select{|p| p.date.year == year }.each do |post|
+        builder << <<HTML
+  <div class="article-link">
+    <h1><a href="/blog/#{post.html_filename}">#{post.title}</a></h1>
+    <h2>Published: #{post.date.strftime "%B %Y"}</h2>
+  </div>
 HTML
+      end
+      builder << "</section>"
     end
     builder
   end
